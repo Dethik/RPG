23f0199350ece7f5a39746f0b09b919de6a7dd9b
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-03 10:54:21 -0800

    good bad fail
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -3,10 +3,10 @@ import Character from "../src/js/character.js"
 describe('Character', () => {
 
   test('Should correctly create PC object', () => {
-    const character = new Character("Brute", Techromancer, SMG, light);
+    const character = new Character("Brute", "Techromancer", "SMG", "light");
     expect(character.name).toEqual("Brute");
-    expect(character.pcClass).toEqual(Techromancer);
-    expect(character.weaponType).toEqual(SMG);
-    expect(character.armorType).toEqual(light);
+    expect(character.pcClass).toEqual("Techromancer");
+    expect(character.weaponType).toEqual("SMG");
+    expect(character.armorType).toEqual("light");
   })
 });
