08ba4bd13deb3f799e4cafa74ed76180622cbf4c
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-03 13:14:15 -0800

    add medic and techromancer class test
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- __tests__/classes.test.js
+++ __tests__/classes.test.js
@@ -2,18 +2,44 @@ import { TestScheduler } from "jest"
 import { soldier, techromancer, medic, scoundrel } from "../src/js/classes.js"
 
 describe('soldier', () => {
-
   test('should apply predefined stat values to soldier class', () => {
-    const soldier = {
-      hp: '20',
-      str: '6',
-      dex: '1',
-      con: '4',
-      int: '1',
-      wis: '1',
-      cha: '2',
-      atk: '2'
-    }
-    expect(soldier).toEqual(soldier);
+    expect(soldier).toEqual({
+      hp: 20,
+      str: 6,
+      dex: 1,
+      con: 4,
+      int: 1,
+      wis: 1,
+      cha: 2,
+      atk: 2
+    })
+  })
+});
+describe('techromancer', () => {
+  test('should check predefined stat values of the techromancer', () => {
+    expect(techromancer).toEqual({
+      hp: 12,
+      str: 1,
+      dex: 3,
+      con: 4,
+      int: 2,
+      wis: 1,
+      cha: 6,
+      atk: 2
+    })
+  })
+});
+describe('medic', () => {
+  test('should check predefined stat values of the medic', () => {
+    expect(medic).toEqual({
+      hp: 16,
+      str: 1,
+      dex: 1,
+      con: 5,
+      int: 3,
+      wis: 6,
+      cha: 2,
+      atk: 1
+    })
   })
 });
--- src/js/classes.js
+++ src/js/classes.js
@@ -1,13 +1,33 @@
 // (Techromancer(cha), Soldier(str), Medic(wis), Scoundrel(dex)) Classes
 
 export const soldier = {
-  hp: '20',
-  str: '6',
-  dex: '1',
-  con: '4',
-  int: '1',
-  wis: '1',
-  cha: '2',
-  atk: '2'
+  hp: 20,
+  str: 6,
+  dex: 1,
+  con: 4,
+  int: 1,
+  wis: 1,
+  cha: 2,
+  atk: 2
 }
-// , techromancer, medic, scoundrel;
+export const techromancer = {
+  hp: 12,
+  str: 1,
+  dex: 3,
+  con: 4,
+  int: 2,
+  wis: 1,
+  cha: 6,
+  atk: 2
+}
+export const medic = {
+  hp: 16,
+  str: 1,
+  dex: 1,
+  con: 5,
+  int: 3,
+  wis: 6,
+  cha: 2,
+  atk: 1
+}
+// scoundrel
