4a22b2c1cfea9dfe292e07e74d1c1b25279b873c
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-03 10:58:57 -0800

    Good Pass Character
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- src/js/character.js
+++ src/js/character.js
@@ -1,5 +1,8 @@
 export default class Character {
   constructor(name, pcClass, weaponType, armorType) {
-
+    this.name = name;
+    this.pcClass = pcClass;
+    this.weaponType = weaponType;
+    this.armorType = armorType;
   }
 }
