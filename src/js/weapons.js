d8681c57c86d510eee3d2c53a89c9f2a4d62bedf
Jon Stump <jmstump@gmail.com>
2021-02-04 10:02:05 -0800

    pass updated weapons test
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- src/js/weapons.js
+++ src/js/weapons.js
@@ -1,32 +1,33 @@
 // (SMG, AR, SG, Melee, Launchers) Weapon Types
 
-class Weapons {}
-export const SMG = {
-  dex: 1,
-  atk: 4,
-  dmg: 5
-}
+export const weapons = {
+  SMG: {
+    dex: 1,
+    atk: 4,
+    dmg: 5
+  },
 
-export const AR ={
-  dex: 2,
-  atk: 5,
-  dmg: 7
-}
+  AR: {
+    dex: 2,
+    atk: 5,
+    dmg: 7
+  },
 
-export const SG = {
-  str: 1,
-  atk: 4,
-  dmg: 2*4
-}
+  SG: {
+    str: 1,
+    atk: 4,
+    dmg: 2*4
+  },
 
-export const Melee = {
-  str: 2,
-  atk: 3,
-  dmg: 7
-}
+  Melee: {
+    str: 2,
+    atk: 3,
+    dmg: 7
+  },
 
-export const Launchers = {
-  wis: 3,
-  atk: 3,
-  dmg: 15
-}
+  Launchers: {
+    wis: 3,
+    atk: 3,
+    dmg: 15
+  }
+};
