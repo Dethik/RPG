6c00211f326cedab1e86476f97fed98d84ae1e87
Jon Stump <jmstump@gmail.com>
2021-02-04 09:41:09 -0800

    add a single classes object and passing test.
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- src/js/classes.js
+++ src/js/classes.js
@@ -1,43 +1,85 @@
 // (Techromancer(cha), Soldier(str), Medic(wis), Scoundrel(dex)) Classes
 
-class Classes {}
-export const soldier = {
-  hp: 20,
-  str: 6,
-  dex: 1,
-  con: 4,
-  int: 1,
-  wis: 1,
-  cha: 2,
-  atk: 2
-}
-export const techromancer = {
-  hp: 12,
-  str: 1,
-  dex: 3,
-  con: 4,
-  int: 2,
-  wis: 1,
-  cha: 6,
-  atk: 3
-}
-export const medic = {
-  hp: 16,
-  str: 1,
-  dex: 1,
-  con: 5,
-  int: 3,
-  wis: 6,
-  cha: 2,
-  atk: 1
-}
-export const scoundrel = {
-  hp: 12,
-  str: 1,
-  dex: 6,
-  con: 3,
-  int: 2,
-  wis: 3,
-  cha: 2,
-  atk: 3
-}
+export const classes = {
+  soldier: {
+    hp: 20,
+    str: 6,
+    dex: 1,
+    con: 4,
+    int: 1,
+    wis: 1,
+    cha: 2,
+    atk: 2
+  },
+  techromancer: {
+    hp: 12,
+    str: 1,
+    dex: 3,
+    con: 4,
+    int: 2,
+    wis: 1,
+    cha: 6,
+    atk: 3
+  },
+  medic: {
+    hp: 16,
+    str: 1,
+    dex: 1,
+    con: 5,
+    int: 3,
+    wis: 6,
+    cha: 2,
+    atk: 1
+  },
+  scoundrel: {
+    hp: 12,
+    str: 1,
+    dex: 6,
+    con: 3,
+    int: 2,
+    wis: 3,
+    cha: 2,
+    atk: 3
+  }
+};
+
+//   export const soldier = {
+//   hp: 20,
+//   str: 6,
+//   dex: 1,
+//   con: 4,
+//   int: 1,
+//   wis: 1,
+//   cha: 2,
+//   atk: 2
+// }
+// export const techromancer = {
+//   hp: 12,
+//   str: 1,
+//   dex: 3,
+//   con: 4,
+//   int: 2,
+//   wis: 1,
+//   cha: 6,
+//   atk: 3
+// }
+// export const medic = {
+//   hp: 16,
+//   str: 1,
+//   dex: 1,
+//   con: 5,
+//   int: 3,
+//   wis: 6,
+//   cha: 2,
+//   atk: 1
+// }
+// export const scoundrel = {
+//   hp: 12,
+//   str: 1,
+//   dex: 6,
+//   con: 3,
+//   int: 2,
+//   wis: 3,
+//   cha: 2,
+//   atk: 3
+// }
